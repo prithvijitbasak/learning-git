2dd7597d57f05908b4668c290b9d72c6e9341868
Prithvijit Basak <prithvijitbasak22695@gmail.com>
2023-06-07 22:01:32 +0530

    Added file sub.cpp to the project folder

---
+++ subtract.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, sub;
+    cout << "Enter 2 numbers"
+         << "\n";
+    cin >> a >> b;
+    sub = a - b;
+    cout << "The difference is " << sub << "\n";
+}
