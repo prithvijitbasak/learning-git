212b9e956738a3c072728d00ae8151411eba2ef1
Prithvijit Basak <prithvijitbasak22695@gmail.com>
2023-06-08 14:55:12 +0530

    Added product files

---
+++ product.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, prod;
+    cout << "Enter two numbers "
+         << "\n";
+    cin >> a >> b;
+    prod = a * b;
+    cout << "The product is " << prod << "\n";
+}
