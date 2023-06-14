4e700740ce70872912aa819c256af3735b39f85f
Prithvijit Basak <prithvijitbasak22695@gmail.com>
2023-06-14 22:00:26 +0530

    Commiting all the files in the path

---
+++ product.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, prod;
+    cout << "Enter three numbers "
+         << "\n";
+    cin >> a >> b >> c;
+    prod = a * b * c;
+    cout << "The product is " << prod << "\n";
+}
---
+++ sum.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, sum = 0;
+    cout<<"Enter two integers"<<"\n";
+    cin>>a>>b;
+    sum = a + b;
+    cout<<"The sum is "<<sum<<"\n";
+}
