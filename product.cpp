4ce836c68b5d2301746e73ba53a969a46e54ca3a
Prithvijit Basak <prithvijitbasak22695@gmail.com>
2023-08-28 06:46:49 +0530

    updated the product file

--- product.cpp
+++ product.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-    int a, b, c, prod;
+    int a, b, c, pro;
     cout << "Enter three numbers "
          << "\n";
     cin >> a >> b >> c;
-    prod = a * b * c;
-    cout << "The product is: " << prod << "\n";
+    pro = a * b * c;
+    cout << "The product is: " << pro << "\n";
 }
